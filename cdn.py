8152be1d3f569b0d41198b8eb00fa1567ef63945
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-03-26 20:21:32 +0000

    Add fastly_force_purge task
    If we default to using soft purges against Fastly, we should still allow a
    forcible purge to be used.

--- cdn.py
+++ cdn.py
@@ -16,6 +16,17 @@ def fastly_purge(*args):
 @task
 @runs_once
 @roles('class-cache')
+def fastly_force_purge(*args):
+    "Forcibly purge items from Fastly, eg \"/one,/two,/three\""
+    govuk_fastly = 'http://www-gov-uk.map.fastly.net'
+    hostnames_to_purge = ['www.gov.uk', 'assets.digital.cabinet-office.gov.uk']
+    for govuk_path in args:
+      for hostname in hostnames_to_purge:
+        run("curl -s -X PURGE -H 'Host: {0}' {1}{2}".format(hostname, govuk_fastly, govuk_path.strip()))
+
+@task
+@runs_once
+@roles('class-cache')
 def purge_all(*args):
     "Purge items from Fastly and cache machines, eg \"/one,/two,/three\""
     cache_purge(*args)
